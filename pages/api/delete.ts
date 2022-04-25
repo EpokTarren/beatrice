888b731cb3e08272b1e6b0706fbba7b9bdd79cbd
Tarren <me@tarren.moe>
2022-04-25 22:07:02 +0200

    Add additional deletion endpoint

--- pages/api/delete.ts
+++ pages/api/delete.ts
@@ -14,21 +14,11 @@ export default endpoint(['DELETE'], async (req, res: NextApiResponse<Output>, se
 
 	if (!req.body.startsWith('/')) return res.status(400).json({ code: 400, message: 'Invalid URL' });
 
-	if (!req.body.startsWith('/' + session.username))
-		return res.status(403).json({ code: 403, message: 'You do not own that file' });
-
-	const file = await prisma.file.findUnique({
-		where: { url: req.body },
-		select: { userId: true },
-	});
-
-	if (!file) return res.status(404).json({ code: 404, message: 'File not found' });
-
-	if (file.userId !== session.uid)
-		return res.status(403).json({ code: 403, message: 'You do not own that file' });
+	if (!req.body.startsWith('/' + session.username + '/'))
+		return res.status(403).json({ code: 403, message: 'You do not own said file, cannot delete.' });
 
 	await prisma.file
 		.delete({ where: { url: req.body } })
 		.then(() => res.status(200).json({ code: 200, message: 'OK' }))
-		.catch(() => res.status(500).json({ code: 500, message: 'Unable to delete file' }));
+		.catch(() => res.status(404).json({ code: 404, message: 'File not found' }));
 });
---
+++ pages/api/delete/[...path].ts
@@ -0,0 +1,26 @@
+import type { NextApiResponse } from 'next';
+import { prisma } from '../../../lib/prisma';
+import { endpoint, EndpointError } from '../../../lib/endpoint';
+
+export interface Success {
+	code: 200;
+}
+
+export type Output = Success | EndpointError;
+
+export default endpoint(['GET', 'DELETE'], async (req, res: NextApiResponse<Output>, session) => {
+	if (!Array.isArray(req.query.path) || req.query.path.length < 2)
+		return res.status(404).json({ code: 404, message: 'File not found' });
+
+	const [user, filename] = req.query.path;
+
+	if (user !== session.username)
+		return res.status(403).json({ code: 403, message: 'You do not own said file, cannot delete.' });
+
+	const url = `/${user}/${filename}`;
+
+	await prisma.file
+		.delete({ where: { url }, select: { url: true } })
+		.then(() => res.status(200).json({ code: 200 }))
+		.catch(() => res.status(404).json({ code: 404, message: 'Unable to find a file to delete' }));
+});

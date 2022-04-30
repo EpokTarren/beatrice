203976ffd163201543f5d151633935c65ba5c066
Tarren <me@tarren.moe>
2022-04-30 11:51:01 +0200

    Fix lib/urls.ts not building

--- lib/urls.ts
+++ lib/urls.ts
@@ -1,7 +1,7 @@
 export const fileServerUrl =
 	process.env.NEXT_PUBLIC_BEATRICE_FILES_URL?.replace(/\/$/, '') ||
-	`${window.location.protocol}//${window.location.host}`;
+	`${window?.location.protocol}//${window?.location.host}`;
 
 export const redirectServerUrl =
 	process.env.NEXT_PUBLIC_BEATRICE_REDIRECT_URL?.replace(/\/$/, '') ||
-	`${window.location.protocol}//${window.location.host}/l`;
+	`${window?.location.protocol}//${window?.location.host}/l`;
